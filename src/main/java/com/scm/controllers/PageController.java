37c84c647471356661878676bc50ff3aee58790a
Aman Srivastava <amanrajasthan123@gmail.com>
2024-06-25 00:49:29 +0530

    Created signup page ui

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -42,4 +42,9 @@ public class PageController {
         return new String("login");
     }
 
+    @GetMapping("/register")
+    public String register(Model model){
+        return "register";
+    }
+
 }
